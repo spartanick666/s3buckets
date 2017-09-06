13df3409b06fae1dcb879ddd764da17de3516a6c
Nick Benos <nbenos@enova.com>
2017-09-06 15:36:41 -0500

    s3_bucketaccess modify

--- s3_bucketaccess.py
+++ s3_bucketaccess.py
@@ -1,8 +1,16 @@
 import boto3
 
-# Create an S3 client
+# Let's use Amazon S3
+s3 = boto3.resource('s3')
+bucket_list = []
+
+
+for x in s3.buckets.all():
+    bucket_list.append(x.name)
+print("\n" .join(bucket_list) + "\n")
+
 s3 = boto3.client('s3')
 
-#Call to S3 to retrieve the policy for the given bucket
-result = s3.get_bucket_acl(Bucket='')
-print(result)
+for y in bucket_list:
+    result = s3.get_bucket_acl(Bucket=y)
+    print(result)
