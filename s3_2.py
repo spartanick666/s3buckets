558c54b9f765b4021019984b2945bf7d7a6b9f6f
Nick Benos <nbenos@enova.com>
2017-09-06 14:13:10 -0500

    s3_2 modify

--- s3_2.py
+++ s3_2.py
@@ -4,12 +4,14 @@ import boto3
 s3 = boto3.resource('s3')
 bucket_list = []
 
+
 for x in s3.buckets.all():
     bucket_list.append(x.name)
-print (bucket_list)
+print("\n" .join(bucket_list) + "\n")
 
 s3 = boto3.client('s3')
 
 for y in bucket_list:
     result = s3.get_bucket_acl(Bucket=y)
     print(result)
+
