d4798b858daaf339788c9cd50b538dbf4163e2bf
Nick Benos <nbenos@enova.com>
2017-09-07 16:18:41 -0500

    s3_mod

--- s3_bucketaccess.py
+++ s3_bucketaccess.py
@@ -1,4 +1,5 @@
 import boto3
+import sys
 
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
@@ -11,6 +12,12 @@ print("\n" .join(bucket_list) + "\n")
 
 s3 = boto3.client('s3')
 
+
 for y in bucket_list:
     result = s3.get_bucket_acl(Bucket=y)
-    print(result)
+    file = open("test2.txt", "a")
+    print(str(result))
+    file.write(str(result))
+    file.close()
+
+
