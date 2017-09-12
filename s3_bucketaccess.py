8da449d27b03ad1de3427bf22aa9d65012f10abb
Nick Benos <nbenos@enova.com>
2017-09-12 16:26:03 -0500

    mod s3

--- s3_bucketaccess.py
+++ s3_bucketaccess.py
@@ -1,23 +1,35 @@
 import boto3
-import sys
 
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
 bucket_list = []
+results_list = []
 
+file = open("s3_bucket_access.txt", "w")
 
 for x in s3.buckets.all():
     bucket_list.append(x.name)
-print("\n" .join(bucket_list) + "\n")
+    #print("\n" .join(bucket_list) + "\n")
+file.write(str("\n" .join(bucket_list) + "\n"))
+
+for number, letter in enumerate(bucket_list):
+    print(number, letter)
+print("\n")
 
 s3 = boto3.client('s3')
 
-
 for y in bucket_list:
     result = s3.get_bucket_acl(Bucket=y)
-    file = open("test2.txt", "a")
-    print(str(result))
-    file.write(str(result))
-    file.close()
+    results_list.append(result)
+    #print(str(result))
+    file.write("\n" + str(result) + "\n")
+file.close()
+for number, letter in enumerate(results_list):
+    print(number, letter)
 
+searchfile = open("s3_bucket_access.txt", "r")
+for line in searchfile:
+        if "global" in line:
+            print("\n The following S3 bucket has public access" + line + "\n")
+searchfile.close()
 
