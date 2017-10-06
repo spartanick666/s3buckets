b36db67b98cd4ed24b1db738f3efbbcb144a9a64
Nick Benos <nbenos@enova.com>
2017-10-06 10:48:23 -0500

    s3_bucketaccess.py modify

--- s3_bucketaccess.py
+++ s3_bucketaccess.py
@@ -1,6 +1,5 @@
 import boto3
 
-# Let's use Amazon S3
 s3 = boto3.resource('s3')
 bucket_list = []
 results_list = []
@@ -10,7 +9,8 @@ file = open("s3_bucket_access.txt", "w")
 for x in s3.buckets.all():
     bucket_list.append(x.name)
     #print("\n" .join(bucket_list) + "\n")
-file.write(str("\n" .join(bucket_list) + "\n"))
+    for i, dest in enumerate(bucket_list, 1):
+        file.write(str("\n" .join(bucket_list) + "\n"))
 
 for number, letter in enumerate(bucket_list):
     print(number, letter)
@@ -28,8 +28,11 @@ for number, letter in enumerate(results_list):
     print(number, letter)
 
 searchfile = open("s3_bucket_access.txt", "r")
+s3_public = open("s3_public.txt", "w")
 for line in searchfile:
         if "global" in line:
             print("\n The following S3 bucket has public access" + line + "\n")
+            s3_public.write("\n The following S3 bucket has public access" + line + "\n")
+s3_public.close()
 searchfile.close()
 
